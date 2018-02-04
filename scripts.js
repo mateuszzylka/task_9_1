410fb932c27363443acbeacf224eb1320d441f21
Mateusz Zylka <mateuszzylkaa@gmail.com>
2018-02-04 22:50:46 +0100

    Add task_9_1

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function getTriangleArea(a, h) {
+	if ((a > 0) && (h > 0)) {
+	}
+	else {
+		console.log('Invalid data');
+	}
+	return a*h/2
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(22, 30);
+var triangle3Area = getTriangleArea(20, 35);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
